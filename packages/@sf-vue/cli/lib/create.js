3639d67f3ba0058e4b5917b927e401076a2bc79f
wxbsuifeng <1533569724@qq.com>
2021-04-13 22:23:04 +0800

    update 更新create文件

--- packages/@sf-vue/cli/lib/create.js
+++ packages/@sf-vue/cli/lib/create.js
@@ -1,4 +1,5 @@
 //fs-extra fs扩展模块
+require('module-alias/register');
 const fs = require('fs-extra')
 const path = require('path')
 
@@ -6,10 +7,76 @@ const path = require('path')
 const inquirer = require('inquirer')
 const Creator = require('./Creator')
 const { clearConsole } = require('./util/clearConsole')
+//插件、依赖模块 的信息： injectPrompt交互， 包名 跟 link
 const { getPromptModules } = require('./util/createTools')
 
 async function create (projectName, options) {
-  console.log(projectName, options);
+  if (options.proxy) {
+    //options.proxy 项目中请求的代理URL
+    process.env.HTTP_PROXY = options.proxy
+  }
+
+  const cwd = options.cwd || process.cwd()  //规定的目录 或者 当前运行的目录
+  const inCurrent = projectName === '.'
+  const name = inCurrent ? path.relative('../', cwd) : projectName
+  const targetDir = path.resolve(cwd, projectName || '.')
+
+  //验证项目名字是否有效
+  const result = validateProjectName(name)
+  if (!result.validForNewPackages) {
+    console.error(chalk.red(`Invalid project name: "${name}"`))
+    result.errors && result.errors.forEach(err => {
+      console.error(chalk.red.dim('Error: ' + err)) //dim？？
+    })
+    result.warnings && result.warnings.forEach(warn => {
+      console.error(chalk.red.dim('Warning: ' + warn))
+    })
+    exit(1)
+  }
+
+  //使用inquirer进行命令行交互 —— 覆盖 合并 取消创建项目
+  if (fs.existsSync(targetDir) && !options.merge) {
+    if (options.force) {
+      await fs.remove(targetDir)
+    } else {
+      await clearConsole()
+      if (inCurrent) {
+        const { ok } = await inquirer.prompt([
+          {
+            name: 'ok',
+            type: 'confirm',
+            message: `Generate project in current directory?`
+          }
+        ])
+        if (!ok) {
+          return
+        }
+      } else {
+        const { action } = await inquirer.prompt([
+          {
+            name: 'action',
+            type: 'list',
+            message: `Target directory ${chalk.cyan(targetDir)} already exists. Pick an action:`,
+            choices: [
+              { name: 'Overwrite', value: 'overwrite' },
+              { name: 'Merge', value: 'merge' },
+              { name: 'Cancel', value: false }
+            ]
+          }
+        ])
+        if (!action) {
+          return
+        } else if (action === 'overwrite') {
+          console.log(`\nRemoving ${chalk.cyan(targetDir)}...`)
+          await fs.remove(targetDir)
+        }
+      }
+    }
+  }
+
+  //getPromptModules  注入特性、注入提示、和用户选择回调
+  const creator = new Creator(name, targetDir, getPromptModules())
+  await creator.create(options)
 }
 
 module.exports = (...args) => {
